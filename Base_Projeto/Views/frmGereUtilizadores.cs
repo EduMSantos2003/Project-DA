1778f2a497db245eca4135894c6f27274e7126db
EduMSantos2003 <2241535@my.ipleiria.pt>
2025-05-05 21:43:51 +0100

    First commit
    Primeiro commit do projeto
    
    Co-Authored-By: AndreLopes98 <184372992+AndreLopes98@users.noreply.github.com>
    Co-Authored-By: sardoruktamov223 <169694335+sardoruktamov223@users.noreply.github.com>

---
+++ Base_Projeto/Views/frmGereUtilizadores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTasks
+{
+    public partial class frmGereUtilizadores : Form
+    {
+        public frmGereUtilizadores()
+        {
+            InitializeComponent();
+            
+            Public string 
+        }
+
+        private void txtIdGestor_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Base_Projeto/Views/frmLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace iTasks
+{
+    public partial class frmLogin : Form
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Username { get; set; }
+        public int Password { get; set; }
+
+        public frmLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            string name = txtUsername.Text;
+            string nif = txtPassword.Text;
+
+        }
+
+    }
+}
