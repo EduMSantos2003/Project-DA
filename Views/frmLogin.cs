44664c304232cabf8c9c2bf23c7b30f69a1b76a0
AndreLopes98 <2241552@my.ipleiria.pt>
2025-05-12 22:07:17 +0100

    Update do check login

--- Views/frmLogin.cs
+++ Views/frmLogin.cs
@@ -16,7 +16,7 @@ namespace iTasks
     public partial class frmLogin : Form
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public int User { get; set; }
         public int Username { get; set; }
         public int Password { get; set; }
 
@@ -28,11 +28,24 @@ namespace iTasks
 
         private void btLogin_Click(object sender, EventArgs e)
         {
+            string name = txtUsername.Text;
+            string password = txtPassword.Text;
             Form secondForm = new frmKanban();
             //secondForm.Show();
 
-            Hide();
-            secondForm.ShowDialog();
+            // Validação Login
+
+            if (name == "abc" && password == "123") 
+            {
+                MessageBox.Show("Login realizado com sucesso!");
+                Hide();
+                secondForm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Login ou senha inválidos!");
+                return;
+            }
         }
 
     }
