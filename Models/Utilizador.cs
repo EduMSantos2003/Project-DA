bdf40a8f233b1828b07d8f36d51d9afc7b7e34c2
sardor <2241538@my.IPLeiria.pt>
2025-05-24 20:13:19 +0100

    Utilizador(24-05-2025)

--- Models/Utilizador.cs
+++ Models/Utilizador.cs
@@ -5,15 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.Security.AccessControl;
+using System.Xml.Linq;
 
 namespace iTasks.Models
 {
     public class Utilizador
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+
+
+        public IList<Utilizador> utilizadores { get; set; }
+
+
+        public Utilizador()
+        {
+            utilizadores = new List<Utilizador>();
+        }
+
+        public Utilizador(string name, string username, string password) : this()
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return Username;
+        }
+
     }
 
+     
+
+
 }
+    
